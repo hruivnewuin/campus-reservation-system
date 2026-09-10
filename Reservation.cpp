674612c805b4bc9c0aa530a588feff624d3f9cba
hruivnewuin <FaithParedes@my.unt.edu>
2026-09-10 13:47:56 -0500

    Add files via upload

---
+++ Reservation.cpp
@@ -0,0 +1,28 @@
+#include "Reservation.h"
+#include <iostream>
+
+Reservation::Reservation()
+    : reservationID(0), studentID(0), studnetName(""), resourceID(""), reservationDate("") {}
+  
+Reservation::Reservation(int resID, int studID, const std::string& studName, const std::string& resourceID, const std::string& resourceID, const std::string& data)
+    : reservationID(resID), studentID(studID), studnetName(studName), resourceID(resourceID), reservationDate(date) {}
+
+int Reservation::getReservationID() const { return reservationID; }
+int Reservation::getStudentID() const { return studentID; }
+std::string Reservation::getStudentName() const { return studentName; }
+std::string Reservation::getResourceID() const { return resourceID; }
+std::string Reservation::getReservationDate() const { return ReservationDate; }
+
+void Reservation::setReservationID(int id) { reservationID = id; }
+void Reservation::setStudentID(int id) { studentID = id; }
+void Reservation::setStudentName(const std::string& name) { studentName = name; }
+void Reservation::setResourceID(const std::string& id) { resourceID = id; }
+void Reservation::setReservationDate(const std::string& date) { ReservationDate = date; }
+
+void Reservation::display() const {
+    std::cout << "Reservation #" << reservationID << " | Student: " << studentName << " (ID " << studentID << ")" << " | Resource: " << resourceID << " | Date: " << reservationDate << std::endl;
+}
+
+
+
+

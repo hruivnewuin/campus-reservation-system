89dd7c8d6b5afd6c3a03fe0c3dc4b063119f5a87
hruivnewuin <FaithParedes@my.unt.edu>
2026-09-19 23:09:09 -0500

    Update Reservation.cpp

--- src/Reservation.cpp
+++ src/Reservation.cpp
@@ -4,7 +4,7 @@
 Reservation::Reservation()
     : reservationID(0), studentID(0), studentName(""), resourceID(""), reservationDate("") {}
   
-Reservation::Reservation(int resID, int studID, const std::string& studName, const std::string& resourceID, const std::string& resourceID, const std::string& date)
+Reservation::Reservation(int resID, int studID, const std::string& studName, const std::string& resourceID,  const std::string& date)
     : reservationID(resID), studentID(studID), studentName(studName), resourceID(resourceID), reservationDate(date) {}
 
 int Reservation::getReservationID() const { return reservationID; }
